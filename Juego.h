6f6c7cc18da84aaf041086fde0d9ebe4b7315876
Prober <ugartegrover@gmail.com>
2022-05-24 20:36:53 -0500

    aleatorios

--- Juego.h
+++ Juego.h
@@ -1,19 +1,27 @@
 #pragma once
 
+#include<random>
+
 class Juego{
 
     public:
     Juego(Juego &other) = delete;
     void operator=(const Juego &) = delete;
     static Juego* getInstance();
-    
+    int generarNumero();
+
     private:
     Juego() = default;
     static Juego *juego;
+    std::random_device rd;
 };
 
 Juego* Juego::getInstance(){
     if(juego == nullptr)
         juego = new Juego();
     return juego;
+}
+
+int Juego::generarNumero(){
+    return rd()%100+1;
 }
